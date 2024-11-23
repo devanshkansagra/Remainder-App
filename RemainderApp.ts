77e472f19ea9dbb28636df57a9b2c40cb68f7c75
Devansh <dkansagra04@gmail.com>
2024-11-23 16:49:28 +0530

    Implemented slash command

--- RemainderApp.ts
+++ RemainderApp.ts
@@ -1,12 +1,18 @@
 import {
     IAppAccessors,
+    IConfigurationExtend,
+    IEnvironmentRead,
     ILogger,
 } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
+import { RemindCommand } from './commands/remainder';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
 
 export class RemainderApp extends App {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
+    }
+    public async extendConfiguration(configuration: IConfigurationExtend, environmentRead: IEnvironmentRead): Promise<void> {
+        configuration.slashCommands.provideSlashCommand(new RemindCommand())
     }
 }
